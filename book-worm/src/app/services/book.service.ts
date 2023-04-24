508ef040528f754a025dc0a6458bab769e3f8a49
kravushapuram <kravushapuram@gmail.com>
2023-04-24 14:43:18 -0500

    Added book description and rating

--- book-worm/src/app/services/book.service.ts
+++ book-worm/src/app/services/book.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, shareReplay } from 'rxjs';
-import { Book } from '../models/book';
+import { Book, BookInfo } from '../models/book';
 import { Categories } from '../models/categories';
 
 @Injectable({
@@ -10,15 +10,21 @@ import { Categories } from '../models/categories';
 export class BookService {
 
   baseURL = 'https://bookcart.azurewebsites.net/api/book/';
+  extraURL: string = '/assets/bookInfo.json';
 
   constructor(private http: HttpClient) { }
 
   categories$ = this.http.get<Categories[]>(this.baseURL + 'GetCategoriesList').pipe(shareReplay(1));
 
   books$ = this.getAllBooks().pipe(shareReplay(1));
+  booksInfo$ = this.getAllBooksInfo().pipe(shareReplay(1));
 
   getAllBooks() {
     return this.http.get<Book[]>(this.baseURL);
+  }
+
+  getAllBooksInfo() {
+    return this.http.get<BookInfo[]>(this.extraURL);
   }
 
   addBook(book) {
@@ -27,6 +33,10 @@ export class BookService {
 
   getBookById(id: number) {
     return this.books$.pipe(map(book => book.find(b => b.bookId === id)));
+  }
+
+  getBookInfoById(id: number) {
+    return this.booksInfo$.pipe(map(book => book.find(b => b.bookId === id)));
   }
 
   getsimilarBooks(bookId: number) {
